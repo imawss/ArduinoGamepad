5119edfe54923fd99239e07d83c4a494633b90bc
Aras Arkan <83475962+ArasArkan@users.noreply.github.com>
2022-11-26 20:49:46 +0300

    Add files via upload

---
+++ SerialControllingTest/SerialControllingTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialControllingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort("COM4", 9600);
+            serialPort.Open();
+            serialPort.ReadTimeout = 1;
+
+            while (serialPort.IsOpen)
+            {
+                try
+                {
+                    switch (serialPort.ReadByte())
+                    {
+                        case 1:
+                            Console.WriteLine("RT");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case 2:
+                            Console.WriteLine("LT");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("You have a error");
+                    break;
+                }
+            }
+        }
+    }
+}
